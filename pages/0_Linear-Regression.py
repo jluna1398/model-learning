92e7f82820ed36a3d7ae9ca0bd027345b014246d
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-17 01:09:49 -0700

    new updategdsdsafdsafsfgsdfsdfs

--- pages/0_Linear-Regression.py
+++ pages/0_Linear-Regression.py
@@ -52,6 +52,5 @@ ad_data = pd.read_csv('data/Advertising.csv').set_index("Unnamed: 0")
 st.dataframe(ad_data.head())
 alt.Chart(ad_data).mark_point().encode(
     x='TV',
-    y='sales',
-    color=alt.condition(brush, alt.value('grey')),
-).add_params(brush)
+    y='sales'
+)
